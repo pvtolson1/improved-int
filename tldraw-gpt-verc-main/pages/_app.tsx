5c8919d8c903ddcdf1f94e94e142d5726ba051a4
pvtolson1 <aolson@synoptek.com>
2025-07-01 07:13:33 -0500

    Delete tldraw-gpt-verc-main directory

--- tldraw-gpt-verc-main/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-// pages/_app.tsx
-import '@tldraw/tldraw/tldraw.css'
-import '../styles/globals.css'
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
